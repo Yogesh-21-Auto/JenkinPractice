6bb4016bfaf8ce893b6578a3452f7438619c2daf
acer <acer@DESKTOP-121C7AT>
2022-08-29 13:00:24 +0530

    B1111

---
+++ src/test/java/gitpractice/git.java
@@ -0,0 +1,11 @@
+package gitpractice;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey Hi Iam Yogi Smarty");
+
+	}
+}
